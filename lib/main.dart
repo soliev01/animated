dd65cc5377c0685cfcfe8cd3dd8dd732ef5242a1
Soliev <abduvositsoliev@gmail.com>
2022-09-23 17:28:52 +0900

    new colors

--- lib/main.dart
+++ lib/main.dart
@@ -41,26 +41,50 @@ class _AnimationScreenState extends State<AnimationScreen>
   late Animation animation;
   final _random = Random();
   late AnimationController _controller;
-  List<Color> colors = [
-    Colors.black,
-    Colors.red,
-    Colors.pink,
-    Colors.purple,
-    Colors.blue,
-    Colors.amber,
-    Colors.teal,
-    Colors.green
+  List<Color> colors = const [
+    Color(0xff55efc4),
+    Color(0xff81ecec),
+    Color(0xff74b9ff),
+    Color(0xffa29bfe),
+    Color(0xffdfe6e9),
+    Color(0xff00b894),
+    Color(0xff0984e3),
+    Color(0xfffd79a8),
+    Color(0xffffeaa7),
+    Color(0xfffab1a0),
+    Color(0xffff7675),
+    Color(0xfffd79a8),
+    Color(0xffd63031),
+    Color(0xfffdcb6e),
+    Color(0xffe17055),
+    Color(0xff2d3436),
+    Color(0xffff6b6b),
+    Color(0xff00d2d3),
+    Color(0xff5f27cd),
+    Color(0xff222f3e),
+    Color(0xff01a3a4),
+    Color(0xffff9ff3),
+    Color(0xff1dd1a1),
+    Color(0xffa55eea),
+    Color(0xffeb3b5a),
+    Color(0xff0fb9b1),
+    Color(0xffa5b1c2),
+    Color(0xfffc5c65),
+    Color(0xfffd9644),
+    Color(0xffcd84f1),
+    Color(0xffff3838),
+    Color(0xff0fbcf9)
   ];
   _getRandomColor() {
-    //return colors[_random.nextInt(colors.length)];
+    return colors[_random.nextInt(colors.length)];
     //return (colors..shuffle(_random)).first;
     //return Colors.primaries[_random.nextInt(Colors.primaries.length)];
-    return Color.fromARGB(
-      _random.nextInt(256),
-      _random.nextInt(256),
-      _random.nextInt(256),
-      _random.nextInt(256),
-    );
+    // return Color.fromARGB(
+    //   _random.nextInt(256),
+    //   _random.nextInt(256),
+    //   _random.nextInt(256),
+    //   _random.nextInt(256),
+    // );
   }
 
   late Color _currentColor;
