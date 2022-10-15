bce702f6d4de1dc45d226bd5e1275fefffd6f5ac
Abdulbasit Soliev <soliev@Abdulbasits-MacBook-Pro.local>
2022-10-15 23:39:44 +0900

    timing and new colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 import 'dart:ui';
-
+import 'colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -41,40 +41,7 @@ class _AnimationScreenState extends State<AnimationScreen>
   late Animation animation;
   final _random = Random();
   late AnimationController _controller;
-  List<Color> colors = const [
-    Color(0xff55efc4),
-    Color(0xff81ecec),
-    Color(0xff74b9ff),
-    Color(0xffa29bfe),
-    Color(0xffdfe6e9),
-    Color(0xff00b894),
-    Color(0xff0984e3),
-    Color(0xfffd79a8),
-    Color(0xffffeaa7),
-    Color(0xfffab1a0),
-    Color(0xffff7675),
-    Color(0xfffd79a8),
-    Color(0xffd63031),
-    Color(0xfffdcb6e),
-    Color(0xffe17055),
-    Color(0xff2d3436),
-    Color(0xffff6b6b),
-    Color(0xff00d2d3),
-    Color(0xff5f27cd),
-    Color(0xff222f3e),
-    Color(0xff01a3a4),
-    Color(0xffff9ff3),
-    Color(0xff1dd1a1),
-    Color(0xffa55eea),
-    Color(0xffeb3b5a),
-    Color(0xff0fb9b1),
-    Color(0xffa5b1c2),
-    Color(0xfffc5c65),
-    Color(0xfffd9644),
-    Color(0xffcd84f1),
-    Color(0xffff3838),
-    Color(0xff0fbcf9)
-  ];
+
   _getRandomColor() {
     return colors[_random.nextInt(colors.length)];
     //return (colors..shuffle(_random)).first;
@@ -109,7 +76,7 @@ class _AnimationScreenState extends State<AnimationScreen>
     super.initState();
     _currentColor = _getRandomColor();
     _controller = AnimationController(
-        duration: const Duration(milliseconds: 1000), vsync: this);
+        duration: const Duration(milliseconds: 700), vsync: this);
     _controller.forward();
   }
 
@@ -153,6 +120,7 @@ class CircularClipper extends CustomClipper<Path> {
     return sqrt(pow(width, 2) + pow(height, 2));
   }
 
+//type simple circular
   @override
   getClip(Size size) {
     final center = centerOffset ?? Offset(size.width / 2, size.height / 2);
@@ -165,6 +133,42 @@ class CircularClipper extends CustomClipper<Path> {
         ),
       );
   }
+
+// 5 star shape
+  // final int peaks = 5;
+
+  // @override
+  // Path getStarClip(Size size) {
+  //   final center = centerOffset ?? Offset(size.width / 2, size.height / 2);
+
+  //   final path = Path();
+
+  //   final innerRadius = lerpDouble(0, maxRadius(size, center), fraction)!;
+  //   final outerRadius = 1.5 * innerRadius;
+
+  //   for (var k = 0; k < peaks; k++) {
+  //     /// radial offset between peaks
+  //     /// coordinates of points are [𝑟cos(2𝜋𝑘/n+𝜋/2), 𝑟sin(2𝜋𝑘/5+𝜋/2)]
+  //     final _bAngle = 2 * pi * k / peaks + pi / 2;
+  //     final _sAngle = 2 * pi * k / peaks + pi / 2 + pi / peaks;
+
+  //     final outerVertices =
+  //         Offset(outerRadius * cos(_bAngle), outerRadius * sin(_bAngle));
+  //     final innerVertices =
+  //         Offset(innerRadius * cos(_sAngle), innerRadius * sin(_sAngle));
+
+  //     if (k == 0) {
+  //       path
+  //         ..moveTo(outerVertices.dx, outerVertices.dy)
+  //         ..lineTo(innerVertices.dx, innerVertices.dy);
+  //     } else {
+  //       path
+  //         ..lineTo(outerVertices.dx, outerVertices.dy)
+  //         ..lineTo(innerVertices.dx, innerVertices.dy);
+  //     }
+  //   }
+  //   return Path()..addPath(path..close(), center);
+  // }
 
   @override
   bool shouldReclip(covariant CustomClipper oldClipper) {
